5dc046cabc0fee4db822d56b34e7f0f278ed46e3
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-16 20:27:08 +0545

    Data types in Python

---
+++ 06type.py
@@ -0,0 +1,15 @@
+num1=10 #int
+num2=10.3 #float
+name="Suna Chaudhary" #string
+names=['Suna','Dhanraj','Sushma'] #list
+salary=[1000.2,2000,30000]
+
+print(type(num1)) #print type of num1 variable
+print(type(num2)) #print type of num2 variable
+print(type(name)) #print type of name variable
+print(type(names)) # print type of names variable
+print(names[1]) #printing the value of names variable with index 1
+print(type(salary))
+print(type(salary[0]))
+print(type(salary[1]))
+print(f"{names[1]}'s salary is {salary[1]}")
