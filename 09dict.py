cf53d4b6a211e383d4ef9fa5cd9553ab97774f88
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-17 20:18:58 +0545

    Dictionary data type

---
+++ 09dict.py
@@ -0,0 +1,9 @@
+address={
+    'Ram':"Bardiya",
+    'Dhanraj':"Tikapur",
+    'Suna':"Nepalgunj"
+}
+
+print(type(address))
+print(f"Address of Ram is {address['Ram']}")
+print(f"Address of Dhanraj is {address['Dhanraj']}")
