0ee3b4a48a29571a3d050ca130ee43de10d50e8b
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-18 21:58:45 +0545

    Age finding by user input

---
+++ 04challenge.py
@@ -0,0 +1,6 @@
+#Age finding by taking user input 
+year_of_birth= int(input("Enter your year of birth:"))
+current_year= int(input("Current year:"))
+
+Age = current_year -year_of_birth
+print(f"Your age is:{Age}")
