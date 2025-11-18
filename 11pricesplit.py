b858c345f0dc1d4513c5fdc02d1fcfced783e18e
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-18 21:48:21 +0545

    user input, price split

---
+++ 11pricesplit.py
@@ -0,0 +1,6 @@
+totalPrice= int(input(print(f"Enter total price:")))
+persons=int(input(print(f"Enter total number of person:")))
+
+individualPrice=totalPrice/persons
+
+print(f"Each person need to pay {individualPrice}")
