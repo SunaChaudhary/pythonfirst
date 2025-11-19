8ced7042ad63e22cc677806e58079ce93fb73816
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-19 22:02:15 +0545

    Learned about membership operators in, not in

---
+++ 17membershipOPerator.py
@@ -0,0 +1,6 @@
+#membership operators: in, not in
+
+fruits=["apple","orange","banana"]
+fruit=input("Enter fruit name:")
+print(fruit in fruits) #returns true if a specified value is found in sequence
+print(fruit not in fruits) #returns true if a specified value is not found in sequence
