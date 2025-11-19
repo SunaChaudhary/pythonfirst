51d7bc61ef944299b9e7de466a1822e29535d94e
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-19 10:06:58 +0545

    operators in python(basic and comparision operators

---
+++ 13operators.py
@@ -0,0 +1,22 @@
+#operators in python
+
+num1=10
+num2=3
+
+#basic operators
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2) #gives integer division
+print(num1%num2) #gives remainder
+print(num1**num2) #num1^num2
+
+
+#comparision operators
+print(num1==num2)
+print(num1!=num2)
+print(num1>num2)
+print(num1<num2)
+print(num1>=num2)
+print(num1<=num2)
