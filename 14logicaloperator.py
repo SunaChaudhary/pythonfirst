4372335a8a7d97df3b545e6154fec7c900fd16a3
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-19 10:36:48 +0545

    diffence between and and or operator- a comment line is added

--- 14logicaloperator.py
+++ 14logicaloperator.py
@@ -6,7 +6,7 @@ user_pin=1020
 inputid=int(input("Enter your user id:"))
 inputpin=int(input("Enter pin:"))
 
-if atm_user_id==inputid and user_pin==inputpin:
+if atm_user_id==inputid and user_pin==inputpin: #if all condition are necessry then we use and , if atleast one conditon is necessary then use or operator
  print("Login successfully")
 else:
  print("Login failed")
