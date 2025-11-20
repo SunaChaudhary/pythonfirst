52bf275356669054969cb1873b16a91cb49f110b
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-20 15:47:05 +0545

    Learned about string method

--- 21stringmethod.py
+++ 21stringmethod.py
@@ -1,4 +1,5 @@
 name= input("Enter your name:")
 print(name.upper())
 print(name.lower())
-print(f"{name.split()[0].capitalize} {name.split()[1].capitalize()}")
+print(f"{name.split()[0].capitalize()} {name.split()[1].capitalize()}")
+
