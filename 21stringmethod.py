5df04207a3a9c1aa4c6589acba8f3c70212945f5
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-20 20:25:21 +0545

    str.title() is used to capitalize first letter of each word in a string

--- 21stringmethod.py
+++ 21stringmethod.py
@@ -1,5 +1,8 @@
+#string method for upper case, lower case and capitalized
 name= input("Enter your name:")
-print(name.upper())
-print(name.lower())
-print(f"{name.split()[0].capitalize()} {name.split()[1].capitalize()}")
+# print(name.upper())
+# print(name.lower())
+# print(f"{name.split()[0].capitalize()} {name.split()[1].capitalize()}")
 
+#String method for capitalize first letter of each word of a string
+print(name.title())
