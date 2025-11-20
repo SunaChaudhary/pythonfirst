e8d656216a18e4b076d3f1b1a19a24bb4dc49e88
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-20 21:49:05 +0545

    .split() string method is learned

--- 21stringmethod.py
+++ 21stringmethod.py
@@ -1,12 +1,19 @@
 #string method for upper case, lower case and capitalized
-name= input("Enter your name:")
+# name= input("Enter your name:")
 # print(name.upper())
 # print(name.lower())
 # print(f"{name.split()[0].capitalize()} {name.split()[1].capitalize()}")
 
 #String method for capitalize first letter of each word of a string
-print(name.title())
-print(name.strip())
+# name= input("Enter your name:")
+# print(name.title())
+# print(name.strip())
 
-Name= "Suna chaudhary Suna"
-print(Name.replace("Suna","Sunu")) #for replace word of a given sequence
+# Name= "Suna, chaudharySuna"
+# print(Name.replace("Suna","Sunu")) #for replace word of a given sequence
+
+#split 
+names="Suna, Sushma, Ram"
+list=names.split(",")
+print(list)
+
