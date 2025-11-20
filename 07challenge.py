98780a203cddf13fea3c3b14115422c8af8c9c65
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-20 22:04:34 +0545

    printing name using title() and strip() string method

---
+++ 07challenge.py
@@ -0,0 +1,5 @@
+#print full name by using string method that makes first letter of each word is capital and remove unnecessary spaces
+
+name= input("Enter your name:")
+Name= name.title().strip()
+print(Name)
