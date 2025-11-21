2cd4cbdbb4d930fde78c02456fd3e1dfc1ef97b3
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-21 20:26:47 +0545

    assert for number which can not be negative

---
+++ 25assert.py
@@ -0,0 +1,13 @@
+#assert
+
+price=int(input("Enter price:"))
+
+assert price>0 , "price must be poitive"
+print("After assert")
+
+#age must be positive so using assert
+
+age=int(input("Enter your age:"))
+
+assert age>0,"Age must be positive"
+print("After assert")
