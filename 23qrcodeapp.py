9176cc684bdc06f532f2909a0939b171269d9f4d
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-21 19:40:52 +0545

    qrcode generation

--- 23qrcodeapp.py
+++ 23qrcodeapp.py
@@ -1,6 +1,11 @@
 #qrcode generation from your name
 import qrcode
-name=input("Enter your name:")
+# name=input("Enter your name:")
+# img=qrcode.make(name)
+# type(img)
+# img.save(f"{name}.png")
+
+name="https://technologychannel.org/"
 img=qrcode.make(name)
 type(img)
-img.save(f"{name}.png")
+img.save("{name}.png")
