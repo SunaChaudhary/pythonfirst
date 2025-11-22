d3615665355d0662b0d3e37fd917043e5916477b
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-22 15:20:08 +0545

    printing name, finding total and average using loop

---
+++ 27names.py
@@ -0,0 +1,21 @@
+#for printing names one by one from a list
+names=["Suna","Sushma","Ram"]
+for name in names:
+    print(name)
+
+#finding total sum and average using for loop
+number=[20,10,30]
+total=0
+n=0
+for i in number:
+    total+=i #finding total like total=total+i
+    n+=1 #increamenting thi divider by 1 each time like n=n+1
+    average=total/(n) #average=total divide by total number of elements
+print(f"Average is {average}")
+print(f"total sum is {total}")
+
+
+
+
+
+
