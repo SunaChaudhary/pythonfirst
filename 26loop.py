69018b73199e62f9e221ba7ef94051292988905b
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-22 14:32:16 +0545

    for loep for repeated task

---
+++ 26loop.py
@@ -0,0 +1,9 @@
+for i in range(1,101): #for printing name 100 times
+    print("Suna Chaudhary")
+
+for i in range(1,101): #for printing number 100 times
+    print(i)
+
+for i in range(1,101): #for peinting even number in between 1 to 100
+    if i%2==0:
+        print(i)
