a3e9fa998b6ba1c34318cd5be3da4553a94801f4
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-24 14:30:32 +0545

    Learned about break and continue

---
+++ 29BreakContinue.py
@@ -0,0 +1,11 @@
+#continue
+
+for i in range(1,101):
+    if i==10:     #skip 10 and continue execution of the code
+        continue
+    print(i)
+
+for i in range(1,101):
+    if i==10:
+        break
+    print(i)
