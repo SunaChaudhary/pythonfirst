b7a64687f65404aa4e95cd6bb499fe7dc5e8ccf4
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-24 15:50:12 +0545

    Found list of mine friend name start with s

---
+++ 11challenge.py
@@ -0,0 +1,27 @@
+#add days to the empty list
+days=[]
+days.append("Sunday")
+
+days.append("Sunday")
+days.append("Monday")
+days.append("Tuesday")
+days.append("wednesday")
+days.append("Thrusday")
+days.append("Friday")
+days.append("Saturday")
+
+print(days)
+
+#creating list of four of mine frinds and findin the names which start with s
+friends=['Dhanraj','Yashoda','Narad', 'Sabina','Karishma','Santoshi']
+print(friends)
+
+for friend in friends:
+    first_char=friend.lower()[0]
+
+    if first_char=='s':
+     print(friend)
+       
+   
+
+   
