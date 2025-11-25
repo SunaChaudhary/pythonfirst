b2b057453e44162aeacd26d686c2a3c3b60174fc
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-25 18:30:30 +0545

    learned about tuple

---
+++ 32tuple.py
@@ -0,0 +1,16 @@
+#basic about tuple
+
+colors=("red","green","yello")
+
+print(colors)
+print(colors[2])
+print(colors[-1])
+#colors[1]="orange" #tuple can not be change means it is used for unchangeable items
+
+#combining tuple
+odd_numbers=(1,3,5)
+even_numbers=(2,4,6)
+all_numbers=odd_numbers+even_numbers
+print(f"The combined Tuple is:{all_numbers}")
+
+
