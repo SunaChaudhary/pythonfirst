0c7b0027531a6693e5b582a60f639e259e89d17d
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-11-30 20:17:53 +0545

    learned about funtion definition and call in python

---
+++ 36function.py
@@ -0,0 +1,13 @@
+#function
+def display(name):
+    print(f"Hello {name}")
+#function call
+display("Suna")
+display("Sushma")
+
+#funtion to find total sum
+def findsum(num1,num2):
+    total=num1+num2
+    print(f"Total sum is: {total}")
+#funtion call
+findsum(10,50)
