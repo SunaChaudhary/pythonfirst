25169ccb87c60f137f9aaf8be60d23b4327f9d27
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-12-02 20:02:10 +0545

    creating directry(folder):make directry and remove directry

---
+++ 49folder.py
@@ -0,0 +1,13 @@
+#creating folder(creating directry)
+import os
+os.mkdir("My Folder")
+# os.mkdir("Your Folder")
+
+#folder remove with exception handling
+
+# try:
+#     os.rmdir("Your Folder")
+# except:
+#     print("folder is not found")
+# finally:
+#     print("program completed finally")
