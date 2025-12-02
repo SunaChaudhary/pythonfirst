06f3014cc96a1828ad6f736d1c3f6f318c84f6de
Suna Chaudhary <chaudharysuna101@gmail.com>
2025-12-02 19:39:18 +0545

    file handling

---
+++ 46readFile.py
@@ -0,0 +1,12 @@
+#file Handling
+#file read
+file=open("hello.txt")
+content=file.read()
+print(content)
+
+#read file which is in another folder
+# file=open("C:/Users/laxma/Desktop/file/name.txt")
+# content=file.read()
+# print(content)
+
+#write to a file
---
+++ 47writeFile.py
@@ -0,0 +1,4 @@
+#write file in python:file handling
+file=open("names.csv",'a') #'a' for append the text
+file.write(" \nSuwash Koirala")
+file.close()
